a7da60a5e3be666d9a30105c0cd87b1b47664459
federico <federico.valente@euris.it>
2019-10-23 10:21:22 +0200

    Initial commit

---
+++ site/components/src/main/java/it/esempio/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package it.esempio.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="alligator:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
